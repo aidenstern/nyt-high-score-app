56587c8e1a5b17b8f8ba0a689460f4e644fe1525
Aiden Stern <aiden.stern@gmail.com>
2023-03-04 18:47:11 -0700

    initial implementation

---
+++ lib/high-score.handler.ts
@@ -0,0 +1,54 @@
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { Handler } from 'aws-lambda';
+
+const s3 = new S3Client({ region: 'us-east-1' });
+
+const bucketName = process.env.BUCKET_NAME || '';
+
+import twillio = require('twilio');
+
+export const handler: Handler = async (event, context) => {
+  try {
+    const twilioSignature = event.headers['x-twilio-signature'];
+    const body = event.body || '';
+
+    const validRequest = twillio.validateRequest(
+      process.env.TWILIO_AUTH_TOKEN,
+      twilioSignature,
+      `${process.env.API_GATEWAY_URL}/sms`,
+      body
+    );
+
+    if (!validRequest) {
+      throw new Error('Invalid request signature');
+    }
+
+    const highScore = parseInt(event.body);
+
+    if (isNaN(highScore)) {
+      throw new Error('Invalid high score');
+    }
+
+    const phoneNumber = event.From.replace(/\D/g, '');
+
+    await s3.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: `${phoneNumber}.txt`,
+        Body: highScore.toString(),
+      })
+    );
+
+    return {
+      statusCode: 200,
+      body: 'High score saved successfully',
+    };
+  } catch (err) {
+    console.error(err);
+
+    return {
+      statusCode: 500,
+      body: 'Error saving high score',
+    };
+  }
+};
--- lib/wordle-high-score-stack.ts
+++ lib/wordle-high-score-stack.ts
@@ -1,16 +1,35 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 
 export class WordleHighScoreStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Step 1: Create an S3 bucket
+    const bucket = new s3.Bucket(this, 'HighScoresBucket', {
+      versioned: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'WordleHighScoreQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Step 2: Create a Lambda function
+    const highScoreHandler = new lambda.Function(this, 'HighScoreHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'high-score.handler',
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+      },
+    });
+
+    bucket.grantReadWrite(highScoreHandler);
+
+    // Step 3: Create an API Gateway REST API
+    const api = new apigateway.RestApi(this, 'HighScoreApi');
+
+    const smsResource = api.root.addResource('sms');
+    const smsIntegration = new apigateway.LambdaIntegration(highScoreHandler);
+    smsResource.addMethod('POST', smsIntegration);
   }
 }
